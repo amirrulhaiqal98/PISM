c44d8d0b2380c2d469583fb0c537f0daa105dd70
amirrulhaiqal98 <amirrulhaiqal98@gmail.com>
2023-06-06 12:07:13 +0800

    ading new controller,views,route for admin and staff

---
+++ app/Http/Controllers/StaffController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StaffController extends Controller
+{
+    public function StaffDashboard(){
+        return view('staff.staff_dashboard');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,9 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\StaffController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -29,3 +32,9 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+//admin
+Route::get('/admin/dashboard', [AdminController::class, 'AdminDashboard'])->name('admin.dashboard');
+
+//staff
+Route::get('/staff/dashboard', [StaffController::class, 'staffDashboard'])->name('staff.dashboard');
