ba4b0eed114c3164306b13afadc69f2a609a34e6
amirrulhaiqal98 <amirrulhaiqal98@gmail.com>
2023-07-24 16:43:29 +0800

    add roles in permission part 4

--- resources/views/backend/pages/rolesetup/add_roles_permission.blade.php
+++ resources/views/backend/pages/rolesetup/add_roles_permission.blade.php
@@ -2,6 +2,12 @@
 @section('admin')
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js">
 </script>
+
+<style type="text/css">
+    .form-check-label{
+        text-transform: capitalize;
+    }
+</style>
     
 <div class="page-content">
 
@@ -25,6 +31,13 @@
                                 @endforeach
 
                             </select>
+                        </div> 
+
+                        <div class="form-group mb-2">
+                            <input type="checkbox" class="form-check-input" id="checkDefaultmain">
+                            <label class="form-check-label" for="checkDefaultmain" >
+                                Permission All
+                            </label>
                         </div> 
 
                         <hr>
@@ -77,5 +90,17 @@
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+
+    $('#checkDefaultmain').click(function(){
+
+        if($(this).is(':checked')){
+            $('input[ type=checkbox]').prop('checked',true);
+        }else{
+            $('input[ type=checkbox]').prop('checked',false);
+        }
+    });
+</script>
 
 @endsection
